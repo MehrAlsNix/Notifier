c9232348044c820aeaf4a26f8658d9deda2f27d2
siad007 <siad.ardroumli@gmail.com>
2015-05-25 15:38:19 +0200

    Initial commit

---
+++ src/Listener.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Notifier
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * @copyright 2015 MehrAlsNix (http://www.mehralsnix.de)
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT
+ * @link      http://github.com/MehrAlsNix/Notifier
+ */
+
+namespace MehrAlsNix\Notifier;
+
+use PHPUnit_Framework_BaseTestListener as BaseTestListener;
+use PHPUnit_Framework_TestSuite as TestSuite;
+
+class Listener extends BaseTestListener
+{
+    public function endTestSuite(TestSuite $suite)
+    {
+        parent::endTestSuite($suite);
+    }
+}
